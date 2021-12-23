fa9d74ac458a5aa8efbd9c526674aef9874b985a
suresh <suresh.sharma510@gmail.com>
2021-12-22 12:33:33 -1200

    second commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,65 +1,46 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enterName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  
+  const enteredNameIsValid = enterName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched ;
 
-  useEffect(() => {
-    if(enteredNameIsValid){
-      console.log('Name input is valid');
-    }
-  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    // console.log(event);
-    // console.log(event.target);
-    // console.log(nameInputRef.current === event.target);
-
-    if (event.target.value.trim() !== ''){
-      setEnteredNameIsValid(true);
-    }
   };
-
+  
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (enterName.trim() === ''){
-      setEnteredNameIsValid(false);    
-    }
   };
-
+  
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
-    if (enterName.trim() === ''){
-      setEnteredNameIsValid(false);
+
+    if (!enteredNameIsValid){
       return;
     }
-
-    setEnteredNameIsValid(true);
     console.log(enterName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
     setEnteredName('');
-    // nameInputRef.current.value='';
+    setEnteredNameTouched(false);
   };
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched ;
+  
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
-
+  
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
           onBlur={nameInputBlurHandler}
           value={enterName}
-        />
+          />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
@@ -70,3 +51,29 @@ const SimpleInput = (props) => {
 };
 
 export default SimpleInput;
+
+
+
+
+
+//*** It is all about ref ***//
+
+// const nameInputRef = useRef();
+// const enteredValue = nameInputRef.current.value;
+// ref={nameInputRef}
+
+// console.log(enteredValue);
+// nameInputRef.current.value='';
+// console.log(nameInputRef.current === event.target);
+// console.log(event);
+// console.log(event.target);
+
+
+
+
+
+// useEffect(() => {
+  //   if(enteredNameIsValid){
+    //     console.log('Name input is valid');
+    //   }
+    // }, [enteredNameIsValid])
